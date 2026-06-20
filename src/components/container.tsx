dc83660851e05da4f29b200306d7b25c072c49ba
Luis Carlos Fernandes Junior <luiscarlosdfernandesjunior@Luiss-MacBook-Air.local>
2026-06-20 11:39:45 +0100

    container layout fixed

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,9 @@
-export function Container ({ children }){
-    return(
-    <div className="main">
-        {children}
-    </div>
-)}
+import type { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export function Container({ children }: ContainerProps) {
+  return <div className="main">{children}</div>;
+}
